f4845d607b5fd9ce24662aacc55ab22132fa0a4e
26info <26info@gmail.com>
2025-10-12 16:35:22 +0300

    Add files via upload

---
+++ plugins/Python_Test_plugin.1h.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# """
+# СУПЕР-МИНИМАЛЬНЫЙ тест для CronBarX
+# """
+
+print("🐍 Test")
+print("---")
+print("✅ Python работает!")
+print("---")
+print("Обновить | refresh=true")
