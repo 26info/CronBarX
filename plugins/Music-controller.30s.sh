d8fc3d099ebffebbc36c1ed500c853e802096061
26info <26info@gmail.com>
2025-10-16 23:31:05 +0300

    Update Music-controller.30s.sh

--- plugins/Music-controller.30s.sh
+++ plugins/Music-controller.30s.sh
@@ -16,19 +16,22 @@ echo "🎵"
 echo "---"
 echo "Универсальные медиа-клавиши"
 echo "---"
-echo "⏮ Предыдущий трек | shell=\"$0\" prev refresh=true"
-echo "⏯ Пауза/Воспр. | shell=\"$0\" playpause refresh=true"
-echo "⏭ Следующий трек | shell=\"$0\" next refresh=true"
+echo "⏮ Предыдущий трек | shell=\"$0\" param1=\"prev\" refresh=true"
+echo "⏯ Пауза/Воспр. | shell=\"$0\" param1=\"playpause\" refresh=true"
+echo "⏭ Следующий трек | shell=\"$0\" param1=\"next\" refresh=true"
 echo "---"
-echo "🔊 Громкость + | shell=\"$0\" volup refresh=true" 
-echo "🔈 Громкость - | shell=\"$0\" voldown refresh=true"
+echo "🔊 Громкость + | shell=\"$0\" param1=\"volup\" refresh=true"
+echo "🔈 Громкость - | shell=\"$0\" param1=\"voldown\" refresh=true"
+echo "🔇 Выкл. звук | shell=\"$0\" param1=\"mute\" refresh=true"
 echo "---"
 echo "ℹ️ Работает с любым плеером"
+echo "🔄 Обновить | refresh=true"
 
-case "$1" in
-    "prev") send_media_key "rewind" ;; # F7
-    "playpause") send_media_key "play" ;; # F8  
-    "next") send_media_key "fastforward" ;; # F9
+case "${1}" in
+    "prev") send_media_key "113" ;; # F7 - предыдущий трек
+    "playpause") send_media_key "107" ;; # F8 - пауза/воспроизведение
+    "next") send_media_key "114" ;; # F9 - следующий трек
     "volup") osascript -e "set volume output volume (output volume of (get volume settings) + 10)" ;;
     "voldown") osascript -e "set volume output volume (output volume of (get volume settings) - 10)" ;;
+    "mute") osascript -e "set volume output volume 0" ;;
 esac
